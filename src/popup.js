96d4db732aa9361ff18adaddbe51407c89aec302
SHINILSUB <deathsizes@hotmail.com>
2021-08-30 19:31:24 +0900

    field

--- src/popup.js
+++ src/popup.js
@@ -8,7 +8,7 @@ this.popUpText = document.querySelector(".pop-up__message");
 this.popUpRefresh = document.querySelector(".pop-up__refresh");
 this.popUpRefresh.addEventListener('click', ()=> {
     this.onClick && this.onClick();
-    hide();
+    this.hide();
 });
 }
 
